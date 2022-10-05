4ab05a8578616ec1c7c8fb260105fc3e73b285a3
Kauan Torrisi <torrisikauan@gmail.com>
2022-10-05 17:52:04 -0300

    example mobx store | view update

---
+++ lib/store/contador.store.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:mobx/mobx.dart';
+part 'contador.store.g.dart';
+
+class ContadorStore = _ContadorStore with _$ContadorStore;
+
+abstract class _ContadorStore with Store {
+  @observable
+  int contador = 0;
+
+  @action
+  void incrementar() {
+    contador++;
+  }
+}
